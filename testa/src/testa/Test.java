5ee7e21a9906122f8b0ea8e868fb39b383db5cbc
hiwatt <hiwatt@DESKTOP-KQ0VIAU>
2018-07-01 11:08:12 +0900

    1st commit

---
+++ testa/src/testa/Test.java
@@ -0,0 +1,10 @@
+package testa;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		for (int i = 0; i < 100; i++) {
+			System.out.println("11111 :: "+i);
+		}
+	}
+}	
---
+++ testa/src/testa/Test2.java
@@ -0,0 +1,15 @@
+package testa;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+	}
+}	
