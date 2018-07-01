993bba622465b6e3ebef695d823ba7d541560b36
hiwatt <hiwatt@DESKTOP-KQ0VIAU>
2018-07-01 11:21:05 +0900

    B-01

--- testa/src/testa/Test2.java
+++ testa/src/testa/Test2.java
@@ -3,13 +3,8 @@ package testa;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
-		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test2");
+		System.out.println("test2");
 	}
 }	
